a9f216d308533ff15413adbbc015b8e1d36031bd
NIKITAGUDOV <140484282+NIKITAGUDOV@users.noreply.github.com>
2023-09-13 19:20:47 +0300

    Add files via upload

---
+++ Gudov_0.3/Gudov_0.3/Gudov_0.3.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+    cout << "Hello World!\n";
+    cout << "Enter your symbol\n";
+    char ans;
+    cin >> ans;
+    cout << "your symbol is " << ans << "\n";
+}
